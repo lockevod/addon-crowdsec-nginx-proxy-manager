d7139f85455cad8aac03e164c8fd804ffa5ec2db
lockevod <58392928+lockevod@users.noreply.github.com>
2024-01-15 21:26:16 +0100

    modify install.sh

--- crowdsec-proxy-manager/crowdsec-bouncer/install.sh
+++ crowdsec-proxy-manager/crowdsec-bouncer/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NGINX_CONF="crowdsec_nginx.conf"
+BOUNCER_PATH="/tmp/crowdsec-bouncer"
 NGINX_CONF_DIR="/etc/nginx/conf.d/"
 
 LIB_PATH="/usr/local/lua/crowdsec/"
@@ -19,16 +20,16 @@ gen_apikey() {
     
     CROWDSEC_LAPI_URL="http://127.0.0.1:${LAPI_DEFAULT_PORT}"
     mkdir -p "${CONFIG_PATH}"
-    API_KEY=${API_KEY} CROWDSEC_LAPI_URL=${CROWDSEC_LAPI_URL} envsubst < ${LUA_MOD_DIR}/config_example.conf | tee -a "${CONFIG_PATH}crowdsec-nginx-bouncer.conf" >/dev/null
+    API_KEY=${API_KEY} CROWDSEC_LAPI_URL=${CROWDSEC_LAPI_URL} envsubst < ${BOUNCER_PATH}/lua-mod/config_example.conf | tee -a "${CONFIG_PATH}crowdsec-nginx-bouncer.conf" >/dev/null
 }
 
 install() {
     mkdir -p ${LIB_PATH}/plugins/crowdsec/
     mkdir -p ${DATA_PATH}/templates/
 
-    cp /tmp/crowdsec-bouncer/nginx/${NGINX_CONF} ${NGINX_CONF_DIR}/${NGINX_CONF}
-    cp -r /tmp/crowdsec-bouncer/lua-mod/lib/* ${LIB_PATH}/
-    cp -r /tmp/crowdsec-bouncer/lua-mod/templates/* ${DATA_PATH}/templates/
+    cp ${BOUNCER_PATH}/nginx/${NGINX_CONF} ${NGINX_CONF_DIR}/${NGINX_CONF}
+    cp -r ${BOUNCER_PATH}/lua-mod/lib/* ${LIB_PATH}/
+    cp -r ${BOUNCER_PATH}/lua-mod/templates/* ${DATA_PATH}/templates/
 
     luarocks install lua-resty-http
     luarocks install lua-cjson
